36f6c5b88cf92d023dfe35ed2c4dcce3c0309675
Tom Trebat <tjtrebat@gmail.com>
2011-06-16 15:41:55 -0400

    started server

--- poker.py
+++ poker.py
@@ -1,45 +1,55 @@
 __author__ = 'Tom'
 
-from cards import *
+import socket
+from server import *
 from tkinter import *
 from tkinter.ttk import *
-
-class PokerHand(Hand):
-    def __init__(self):
-        super(PokerHand, self).__init__()
-
-class Poker:
-    def __init__(self):
-        self.deck = Deck(count=2)
-        self.deck.shuffle()
-        self.players = []
-
-    def add_player(self):
-        self.players.append(PokerHand())
-
-    def deal_card(self, player):
-        player.add(self.deck.cards.pop())
-
-    def __str__(self):
-        s = ""
-        for i, player in enumerate(self.players):
-            s += "Player %d: %s\n" % (i + 1, str(player))
-        return s
 
 class PokerGUI:
     def __init__(self, root):
         self.root = root
-        self.poker = Poker()
-        self.poker.add_player()
+        self.root.title("Poker")
+        self.root.geometry("800x550")
+        self.canvas = Canvas(self.root, width=800, height=500)
+        self.canvas.pack(fill='both', expand='yes')
+        self.face_down_image = PhotoImage(file="cards/b1fv.gif")
+        self.connect_to_server()
+        self.cards = {}
+        for card in self.server.poker.deck.cards:
+            self.cards[card] = PhotoImage(file=card.get_image())
+        self.images = {player: [] for player in self.server.poker.players}
         self.new_game()
-        print(self.poker)
+        #print(self.poker)
 
     def new_game(self):
         for i in range(2):
             for player in self.poker.players:
                 self.poker.deal_card(player)
+                position = player.get_position()
+                if i > 0:
+                    if not player.position:
+                        position = (position[0] - 75, position[1])
+                    else:
+                        position = (position[0] - 10, position[1] - 10)
+                if not player.position:
+                    image = self.cards[player.cards[-1]]
+                else:
+                    image = self.face_down_image
+                self.images[player].append(self.canvas.create_image(position, image=image))
+                #self.canvas.itemconfig(self.images[player][-1], image=self.cards[player.cards[-1]])
+        #self.bet = Scale(self.root, from_=0, to=)
+
+    def connect_to_server(self):
+        HOST = socket.gethostname()    # The remote host
+        PORT = 50007              # The same port as used by the server
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((HOST, PORT))
+
+    def get_position(self, position):
+        positions = ((435, 450), (50, 450), (50, 250), (50, 60), (400, 60), (750, 60), (750, 250), (750, 450),)
+        return positions[position]
 
 if __name__ == "__main__":
     root = Tk()
-    PokerGUI(root)
+    gui = PokerGUI(root)
     root.mainloop()
